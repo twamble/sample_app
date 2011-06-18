87a067a913ada2a4695e2f5e94016dee390643e3
twamble <travis@yupe.org>
2011-06-18 17:22:25 -0500

    Added Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+  match '/about', :to => 'pages#about'
   
-  get "pages/about"
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
